1d6419005a54833b3df6de0f2028711f0a326078
Igor Boyko <iboyko@gmail.com>
2023-01-10 16:20:37 -0800

    working app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { actions } from './store/index';
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
   const increment = () => {
-    dispatch({type: 'INC'});
+    dispatch(actions.increment());
   };
   const decrement = () => {
-    dispatch({type: 'DEC'});
+    dispatch(actions.decrement());
   }
   const addValue = () => {
-    dispatch({type: 'ADD', payload: 10});
+    dispatch(actions.addValue(10));
   }
 
   return (
     <div className='App'>
-      <div className='App-header'>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
       <button onClick={addValue}>Add 10</button>
-      </div>
     </div>
   );
 }
